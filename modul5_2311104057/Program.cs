39cdc3f66a2f702bfe922f51bde521396d509efe
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-03-20 22:00:22 +0700

    menambahkan class SimpleDataBase

--- modul5_2311104057/Program.cs
+++ modul5_2311104057/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.UtcNow);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SimpleDataBase<int> database = new SimpleDataBase<int>();
+        database.AddNewData(23);
+        database.AddNewData(11);
+        database.AddNewData(10);
+
+        database.PrintAllData();
+    }
+}
